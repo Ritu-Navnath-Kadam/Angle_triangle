bdd5b51e27cac1d9c0592664bd655a5f8408475e
Ritu-Navnath-kadam <144406164+Ritu-Navnath-Kadam@users.noreply.github.com>
2023-10-23 15:38:07 +0530

    Create Angle_triangle.c

---
+++ Angle_triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+
+float a,b,c;
+printf("Enter 1st angle of triangle");
+scanf("%f",&a);
+
+
+printf("Enter 2nd angle of triangle");
+scanf("%f",&b);
+
+c=180-(a+b);
+
+printf("The 3rd angle of triangle is =%f",c);
+
+}
